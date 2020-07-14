87323f3acfd917dc9a8629c5b1d7453483d5c2ee
karan-tewari <ktewari601@gmail.com>
2020-07-15 01:45:30 +0530

    simple redux i index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,44 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {createStore} from 'redux';
+
+//actions
+const increment = () => {
+  return {
+    type : 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return{
+    type: 'DECREMENT'
+  }
+}
+
+//reducers
+
+const reducer = (state = 0, action) => {
+    switch(action.type){
+      case 'INCREMENT' : 
+        return state + 1;
+      case 'DECREMENT' :
+        return state - 1;
+      default : 
+        return state;
+    }
+}
+
+
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
+ReactDOM.render(<App />,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
