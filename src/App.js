ef888f4569d861868a240693cd71d8068baa8e3e
karan-tewari <ktewari601@gmail.com>
2020-07-17 23:59:24 +0530

    dev tools and state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import {useSelector} from 'react-redux';
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state=> state.isLogged)
+
   return (
     <div className="App">
-      <h2>Hello</h2>
-    </div>
+      <h2>Hello {counter}</h2>
+      {isLogged ? <h1>This is personal info</h1> : <h1>Cant show</h1>}
+      </div>
   );
 }
 
